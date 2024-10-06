8b0c2b089ca9d514400441ff2c4508fb2ffdbed5
PandaGuerrier <76222146+PandaGuerrier@users.noreply.github.com>
2024-10-06 20:07:34 +0200

    feat: remove guards options

--- app/middleware/auth_middleware.ts
+++ app/middleware/auth_middleware.ts
@@ -1,6 +1,5 @@
 import type { HttpContext } from '@adonisjs/core/http'
 import type { NextFn } from '@adonisjs/core/types/http'
-import type { Authenticators } from '@adonisjs/auth/types'
 
 /**
  * Auth middleware is used authenticate HTTP requests and deny
@@ -15,9 +14,6 @@ export default class AuthMiddleware {
   async handle(
     { auth }: HttpContext,
     next: NextFn,
-    options: {
-      guards?: (keyof Authenticators)[]
-    } = {}
   ) {
     await auth.use('web').authenticate()
     return next()
