8814d32f92f25c392409daf440c6743652a4879f
PandaGuerrier <76222146+PandaGuerrier@users.noreply.github.com>
2024-12-03 21:38:00 +0100

    feat: py support

--- inertia/pages/connected/subjects/Show.tsx
+++ inertia/pages/connected/subjects/Show.tsx
@@ -1,4 +1,3 @@
-import useAuth from '~/hooks/use_auth'
 import DashboardLayout from '~/layouts/DashboardLayout'
 import { Head } from '@inertiajs/react'
 import { InferPageProps } from '@adonisjs/inertia/types'
@@ -6,11 +5,8 @@ import SubjectsController from '#controllers/common/users/subjects_controller'
 import Exercise from '#models/exercise'
 import { Button } from '@nextui-org/react'
 
+// @ts-ignore
 export default function SubjectShow({ subject }: InferPageProps<SubjectsController, 'show'>) {
-  const auth = useAuth()
-  console.log(auth)
-  console.log(subject)
-
   return (
     <DashboardLayout>
       <Head title={subject!.defaultSubject.title}/>
@@ -21,17 +17,17 @@ export default function SubjectShow({ subject }: InferPageProps<SubjectsControll
           Ici, vous trouverez tous les sujets où vous avez été assignés !
         </div>
 
+
         <div className={'flex text-white space-x-5'}>
           {
             subject!.exercises.map((exercise: Exercise) => {
               return (
-                <div key={exercise.id} className={'text-white border border-gray-500 p-5 rounded-md hover:bg-gray-800 duration-200 flex'}>
+                <div key={exercise.id} className={'text-white border p-5 rounded-md hover:bg-gray-800 duration-200 flex ' + (exercise.isFinish ? 'border-green-500' : 'border-red-500')}>
                   <div>
                     <div className={'font-bold'}>{exercise.defaultExercice.title}</div>
                     <div>{exercise.defaultExercice.description}</div>
                     <Button className={'mt-5'} variant={'light'} color={'primary'} fullWidth href={'/subjects/' + exercise.subjectId + '/exercises/' + exercise.id} as={'a'}>Commencer</Button>
                   </div>
-                  <div className={'font-bold ' + (exercise.isFinish ? 'text-green-500' : 'text-gray-50')}>{ exercise.isFinish ? 'Fini !' : 'Encore du travail :('}</div>
                 </div>
               )
             })
